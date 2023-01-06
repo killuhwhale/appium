0c29534e69db9498b4cf15168d49aba4117c865c
killuh <20297127+killuhwhale@users.noreply.github.com>
2023-01-05 20:27:34 -0800

    Unicode fix, Error SS added

--- src/main.py
+++ src/main.py
@@ -9,46 +9,48 @@ from utils.utils import (PLAYSTORE_MAIN_ACT, PLAYSTORE_PACKAGE_NAME,
 
 # Multiprocessing Runs
 
-# ips = [
-#     # '192.168.1.238:5555',
-#     # '710KPMZ0409387',  # Device connected via USB (Pixel 2)
-#     '192.168.1.113:5555',
-#     # 'emulator-5554',
+ips = [
+    # '710KPMZ0409387',  # Device connected via USB (Pixel 2)
+    # 'emulator-5554',
+    '192.168.1.238:5555',  # Helios x86 intel ARC-R
+    '192.168.1.113:5555', # CoachZ snapdragon ARC-P
+    '192.168.1.248:5555', # Morphius AMD ARC-P
+    '192.168.1.128:5555',  # Kevin ARM ARC-p
 
-# ]
-# runner = MultiprocessTaskRunner(ips, TOP_500_APPS[0:4])
-# runner.run()
+]
+runner = MultiprocessTaskRunner(ips, TOP_500_APPS[0:4])
+runner.run()
 
 ###################################
 #   Single run
 ###################################
 
-ip = '710KPMZ0409387' # ARC-R
-ip = 'emulator-5554' # ARC-R
-ip = '192.168.1.113:5555' # ARC-P
-ip = '192.168.1.238:5555' # ARC-R Helios, Failing on install step ##*#*#*#*#*#**#*##*
-ip = '192.168.1.125:5555' # ARC-R Helios, Failing on install step ##*#*#*#*#*#**#*##*
+# ip = '710KPMZ0409387' # ARC-R
+# ip = 'emulator-5554' # ARC-R
+# ip = '192.168.1.113:5555' # ARC-P
+# ip = '192.168.1.238:5555' # ARC-R Helios, Failing on install step ##*#*#*#*#*#**#*##*
+# ip = '192.168.1.125:5555' # ARC-R Helios, Failing on install step ##*#*#*#*#*#**#*##*
 
-res = adb_connect(ip)
-transport_id = find_transport_id(ip)
-version = get_arc_version(transport_id)
+# res = adb_connect(ip)
+# transport_id = find_transport_id(ip)
+# version = get_arc_version(transport_id)
 
-driver = webdriver.Remote(
-    "http://localhost:4723/wd/hub",
-    android_des_caps(
-        ip,
-        PLAYSTORE_PACKAGE_NAME,
-        PLAYSTORE_MAIN_ACT
-    )
-)
-driver.implicitly_wait(5)
-driver.wait_activity(PLAYSTORE_MAIN_ACT, 5)
+# driver = webdriver.Remote(
+#     "http://localhost:4723/wd/hub",
+#     android_des_caps(
+#         ip,
+#         PLAYSTORE_PACKAGE_NAME,
+#         PLAYSTORE_MAIN_ACT
+#     )
+# )
+# driver.implicitly_wait(5)
+# driver.wait_activity(PLAYSTORE_MAIN_ACT, 5)
 
-validator = AppValidator(driver, TOP_500_APPS[:1], transport_id, version, ip)
-validator.uninstall_multiple()
-validator.run()
-validator.report.print_report()
-driver.quit()
+# validator = AppValidator(driver, TOP_500_APPS[:1], transport_id, version, ip)
+# validator.uninstall_multiple()
+# validator.run()
+# validator.report.print_report()
+# driver.quit()
 
 
 
