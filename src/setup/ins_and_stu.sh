33585df0e75e632ecbc5ea2ddd1245e7a28b8b3c
killuh <20297127+killuhwhale@users.noreply.github.com>
2023-01-06 20:05:56 -0800

    Updated setup scripts

--- src/setup/ins_and_stu.sh
+++ src/setup/ins_and_stu.sh
@@ -1,7 +1,7 @@
 #!/user/bin/bash
 
 
-if [ -f "/opt/android-studio" ]
+if [ -e "/opt/android-studio" ]
 then
     # The file exists
     echo "Android studio already installed."
@@ -12,18 +12,21 @@ fi
 echo "Fetching Android Studio"
 
 # Link from studio website
-# curl https://redirector.gvt1.com/edgedl/android/studio/install/2021.2.1.15/android-studio-2021.2.1.15-cros.deb
+# curl https://redirector.gvt1.com/edgedl/android
+#   /studio/install/2021.2.1.15/android-studio-2021.2.1.15-cros.deb
 AS_DL_URL=$(curl "https://developer.android.com/studio/index.html#downloads" | grep -o -m 1 --regexp='https.*\.deb')
 echo "Found DL Link: $AS_DL_URL"
 
 # Regex Extract
 # The document has moved
-# <A HREF="https://r3---sn-o097znss.gvt1.com/edgedl/android/studio/install/2021.2.1.15/android-studio-2021.2.1.15-cros.deb?cms_redirect=yes&amp;mh=D\
--&amp;mip=192.145.118.165&amp;mm=28&amp;mn=sn-o097znss&amp;ms=nvh&amp;mt=1654888163&amp;mv=u&amp;mvi=3&amp;pl=24&amp;shardbypass=sd&amp;smhost=r5---\
-sn-o097znze.gvt1.com">here</A>.
+# <A HREF="https://r3---sn-o097znss.gvt1.com/edgedl/android/studio
+#   /install/2021.2.1.15/android-studio-2021.2.1.15-cros.deb?cms_redirect=yes&amp;mh=D\
+#   -&amp;mip=192.145.118.165&amp;mm=28&amp;mn=sn-o097znss&amp;ms=nvh&amp;mt=1654888163&amp;mv=u&amp;mvi=3&amp;pl=24&amp;shardbypass=sd&amp;smhost=r5---\
+#    sn-o097znze.gvt1.com">here</A>.
 
-# STUDIO=$(curl https://redirector.gvt1.com/edgedl/android/studio/install/2021.2.1.15/android-studio-2021.2.1.15-cros.deb | grep -o --regexp='https.\
-*com')
+# STUDIO=$(curl https://redirector.gvt1.com/edgedl/android
+#   /studio/install/2021.2.1.15/android-studio-2021.2.1.15-cros.deb
+#    | grep -o --regexp='https.\*com')
 STUDIO=$(curl $AS_DL_URL | grep -o --regexp='https.*com')
 
 echo "Downloading from: $STUDIO..."
--- src/setup/setup.sh
+++ src/setup/setup.sh
@@ -1,19 +1,27 @@
 # echo "password" | sudo -S myscript.sh
 # Install Java
 sudo apt install default-jre
-apt install libnss3-dev libgdk-pixbuf2.0-dev libgtk-3-dev libxss-dev
+sudo apt install libnss3-dev libgdk-pixbuf2.0-dev libgtk-3-dev libxss-dev
 
 # Setup Node environment
-touch ~/.bashrc
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-source ~/.bashrc
-nvm install 18.7.0
-nvm use node
-npm install -g appium
+
+num_files=$(ls -1a $NVM_DIR | wc -l)
+if [ $num_files -gt 0 ]; then
+  # If nvm is installed, print "Installed"
+  echo "Installed"
+else
+    # If nvm is not installed, print "Not installed"
+    echo "Not installed"
+    touch ~/.bashrc
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+    source ~/.bashrc
+    nvm install 18.7.0
+    nvm use node
+    npm install -g appium
+fi
+
 
 bash ins_and_stu.sh
-
-
 
 bash append_to_bashrc.sh "export ANDROID_HOME=/home/$USER/Android/Sdk"
 bash append_to_bashrc.sh "export PATH=$PATH:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$ANDROID_HOME/build-tools;"
@@ -23,12 +31,17 @@ bash append_to_bashrc.sh "export PATH=$PATH:$JAVA_HOME"
 
 
 
+if [ ! -e "/home/$USER/appium" ]; then
+    sudo apt-get install python3-venv
+    git clone https://github.com/killuhwhale/appium.git
+    cd appium
+    python3 -m venv .
+    source bin/activate
+    cd src
+    pip install -r requirements.txt
+else
+    cd appium/src
+fi
 
 
-sudo apt-get install python3-venv
-git clone https://github.com/killuhwhale/appium.git
-cd appium
-python3 -m venv .
-source bin/activate
-cd src
-pip install -r requirements.txt
+python3 main.py
