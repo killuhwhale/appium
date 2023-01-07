486eac943f04c84332255d052aa5033bda94a2fd
killuh <20297127+killuhwhale@users.noreply.github.com>
2023-01-06 20:48:09 -0800

    Setup updates

--- src/setup/readme.md
+++ src/setup/readme.md
@@ -1,3 +1,11 @@
 Collection of scripts to use on host machine to setup environment
 
 Drag all files to root linux dir on Chromebook and run "bash setup.sh"
+
+# First run
+bash ins_and_stu.sh
+
+# Once Android Studio opens, install Sdk (complete init setup)
+
+# Then, run
+bash setup.sh
--- src/setup/setup.sh
+++ src/setup/setup.sh
@@ -14,7 +14,9 @@ else
     echo "Not installed"
     touch ~/.bashrc
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-    source ~/.bashrc
+
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
     nvm install 18.7.0
     nvm use node
     npm install -g appium
@@ -23,10 +25,10 @@ fi
 
 bash ins_and_stu.sh
 
-bash append_to_bashrc.sh "export ANDROID_HOME=/home/$USER/Android/Sdk"
-bash append_to_bashrc.sh "export PATH=$PATH:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$ANDROID_HOME/build-tools;"
-bash append_to_bashrc.sh "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64"
-bash append_to_bashrc.sh "export PATH=$PATH:$JAVA_HOME"
+bash append_to_bashrc.sh "export ANDROID_HOME=/home/\$USER/Android/Sdk" "/home/$USER/.bashrc"
+bash append_to_bashrc.sh "export PATH=\$PATH:\$ANDROID_HOME/platform-tools:\$ANDROID_HOME/tools:\$ANDROID_HOME/build-tools;" "/home/$USER/.bashrc"
+bash append_to_bashrc.sh "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64" "/home/$USER/.bashrc"
+bash append_to_bashrc.sh "export PATH=\$PATH:\$JAVA_HOME"
 
 
 
